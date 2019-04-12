d0202973a21dbba8a7b391dc01b9ca5f4dae3a45
SolarWind23 <fogebogeololo@gmail.com>
2019-04-12 14:30:32 +0300

    create form on dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,30 @@ import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRouters} from './app.routes';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {DataService} from './data/data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
+    AppRouters,
+    MatGridListModule,
   ],
-  providers: [],
+
+  providers: [DataService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,23 +1,38 @@
 import {NgModule} from '@angular/core';
 import {
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-} from '@angular/material';
+
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonModule,
   
+
+} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   imports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
+
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTableModule,
+    
 ],
   exports: [
+
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    
   ],
 })
 export class MaterialModule {};
